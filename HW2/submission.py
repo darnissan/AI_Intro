7b532fc49804db4890bb6759b84b15b76ecbab1a
darnissan <67154616+darnissan@users.noreply.github.com>
2024-03-10 17:16:58 +0200

    first part of HW2

---
+++ HW2/submission.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from Agent import Agent, AgentGreedy
+from WarehouseEnv import WarehouseEnv, manhattan_distance
+import random
+
+
+
+def smart_heuristic_for_robot (env: WarehouseEnv, robot_id : int) :
+    robot = env.get_robot(robot_id)
+
+    max = -np.inf
+    if (robot.package is None):
+        for p in env.packages:
+            if p.on_board:
+                current = 2 * manhattan_distance(p.position, p.destination) - manhattan_distance(p.position,
+                                                                                                 robot.poistion)
+            if current > max:
+                max = current
+    else:
+        max = 2 * manhattan_distance(robot.package.position, robot.package.destination)
+
+    min = np.inf
+    for c in env.charge_stations:
+        current = manhattan_distance(c.position, robot.poistion)
+        if current < min:
+            min = current
+    h_robot = robot.credit + max - min
+    return h_robot
+
+# TODO: section a : 3
+def smart_heuristic(env: WarehouseEnv, robot_id: int):
+    robot_h=smart_heuristic_for_robot(env,robot_id)
+    other_robot_h=smart_heuristic_for_robot(env,(robot_id-1)%2)
+    return robot_h - other_robot_h
+
+
+class AgentGreedyImproved(AgentGreedy):
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+
+
+        return smart_heuristic(env, robot_id)
+
+
+class AgentMinimax(Agent):
+    # TODO: section b : 1
+    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
+        raise NotImplementedError()
+
+
+class AgentAlphaBeta(Agent):
+    # TODO: section c : 1
+    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
+        raise NotImplementedError()
+
+
+class AgentExpectimax(Agent):
+    # TODO: section d : 1
+    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
+        raise NotImplementedError()
+
+
+# here you can check specific paths to get to know the environment
+class AgentHardCoded(Agent):
+    def __init__(self):
+        self.step = 0
+        # specifiy the path you want to check - if a move is illegal - the agent will choose a random move
+        self.trajectory = ["move north", "move east", "move north", "move north", "pick_up", "move east", "move east",
+                           "move south", "move south", "move south", "move south", "drop_off"]
+
+    def run_step(self, env: WarehouseEnv, robot_id, time_limit):
+        if self.step == len(self.trajectory):
+            return self.run_random_step(env, robot_id, time_limit)
+        else:
+            op = self.trajectory[self.step]
+            if op not in env.get_legal_operators(robot_id):
+                op = self.run_random_step(env, robot_id, time_limit)
+            self.step += 1
+            return op
+
+    def run_random_step(self, env: WarehouseEnv, robot_id, time_limit):
+        operators, _ = self.successors(env, robot_id)
+
+        return random.choice(operators)
